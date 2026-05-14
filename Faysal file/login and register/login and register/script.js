4345d2ef68b54bb1f5018f3e48bdd3695d028db8
Mahmudul-UIU23 <mahmudulmehedi23@gmail.com>
2026-05-14 22:33:52 +0600

    last week all file added

---
+++ Faysal file/login and register/login and register/script.js
@@ -0,0 +1,21 @@
+// ========================================
+// Register page e jawar function
+// ========================================
+function goToRegisterPage(){
+
+  // Login page theke register.html open hobe
+  window.location.href = "register.html";
+
+}
+
+
+
+// ========================================
+// Login page e jawar function
+// ========================================
+function goToLoginPage(){
+
+  // Register page theke index.html open hobe
+  window.location.href = "index.html";
+
+}
