6914999aa38ea5a114b7417fc066616248f8f39d
Shafayat Islam <shafayatislamsaad@gmail.com>
2026-05-23 12:24:33 +0600

    Add existsByEmail() method to PatientDetailsRepo and DoctorDetailsRepo

--- virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/repository/DoctorDetailsRepo.java
+++ virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/repository/DoctorDetailsRepo.java
@@ -4,4 +4,5 @@ import com.code.virtual_hospital_v1.model.DoctorDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorDetailsRepo extends JpaRepository<DoctorDetails, Long> {
+    boolean existsByEmail(String email);
 }
--- virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/repository/PatientDetailsRepo.java
+++ virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/repository/PatientDetailsRepo.java
@@ -4,4 +4,5 @@ import com.code.virtual_hospital_v1.model.PatientDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientDetailsRepo extends JpaRepository<PatientDetails, Long> {
+    boolean existsByEmail(String email);
 }
