c05c6dc864033bb0b0afe9627134af100b6da636
Shafayat Islam <shafayatislamsaad@gmail.com>
2026-05-24 23:27:56 +0600

    Modify DoctorService and DoctorController

--- virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/controller/DoctorController.java
+++ virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/controller/DoctorController.java
@@ -1,8 +1,8 @@
 package com.code.virtual_hospital_v1.controller;
 
 import com.code.virtual_hospital_v1.dto.DoctorDetailsRequest;
+import com.code.virtual_hospital_v1.dto.DoctorDetailsResponse;
 import com.code.virtual_hospital_v1.service.DoctorService;
-import com.code.virtual_hospital_v1.service.PatientService;
 import com.code.virtual_hospital_v1.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +25,17 @@ public class DoctorController {
     public ResponseEntity<String> doctorRegistration(@RequestBody DoctorDetailsRequest request){
         userService.registerDoctor(request);
         return new ResponseEntity<>("Registration successful", HttpStatus.CREATED);
+    }
+
+    @GetMapping("doctor/details/{id}")
+    public ResponseEntity<DoctorDetailsResponse> getDoctorDetails(@PathVariable Long id){
+        DoctorDetailsResponse response = doctorService.getDoctorDetails(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PutMapping("doctor/details/{id}")
+    public ResponseEntity<String> updateDoctorDetails(@PathVariable Long id, @RequestBody DoctorDetailsRequest request){
+        doctorService.updateDoctorDetails(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body("Details Updated Successfully.");
     }
 }
--- virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/service/DoctorService.java
+++ virtual_hospital_v1/src/main/java/com/code/virtual_hospital_v1/service/DoctorService.java
@@ -1,6 +1,8 @@
 package com.code.virtual_hospital_v1.service;
 
 import com.code.virtual_hospital_v1.dto.DoctorDetailsRequest;
+import com.code.virtual_hospital_v1.dto.DoctorDetailsResponse;
+import com.code.virtual_hospital_v1.exception.UserNotFoundException;
 import com.code.virtual_hospital_v1.model.DoctorDetails;
 import com.code.virtual_hospital_v1.repository.DoctorDetailsRepo;
 import com.code.virtual_hospital_v1.repository.UserRepo;
@@ -17,4 +19,28 @@ public class DoctorService {
         this.doctorRepo = doctorRepo;
     }
 
+    public DoctorDetailsResponse getDoctorDetails(Long id){
+        DoctorDetails details = doctorRepo.findById(id).orElseThrow(() -> new UserNotFoundException());
+        return DoctorDetailsResponse.fromDoctorDetails(details);
+    }
+
+    public void updateDoctorDetails(Long id, DoctorDetailsRequest request){
+        DoctorDetails details = doctorRepo.findById(id).orElseThrow(() -> new UserNotFoundException());
+
+        if(request.getConsultationFee() < 0)
+            throw new IllegalArgumentException("Consultation fee can not be negative!");
+
+        details.setFullName(request.getFullName());
+        details.setEmail(request.getEmail());
+        details.setPhone(request.getPhone());
+        details.setGender(request.getGender());
+        details.setExperience(request.getExperience());
+        details.setSpecialization(request.getSpecialization());
+        details.setQualification(request.getQualification());
+        details.setAbout(request.getAbout());
+        details.setConsultationFee(request.getConsultationFee());
+        details.setPicUrl(request.getPicUrl());
+
+        doctorRepo.save(details);
+    }
 }
